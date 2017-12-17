c17e072cdd501adef282b8ee2cbf95afb6549bc7
Rogonov Stepan <rogonovstepan@gmail.com>
2017-12-17 14:00:36 +0300

    Done writing floating point serialization tests

--- lib/floating-point-serialization/test/main.c
+++ lib/floating-point-serialization/test/main.c
@@ -47,7 +47,7 @@ NEW_TEST(type_float_regular)
 			deserialize_float(
 				  &(actual_val[i])
 				, actual_mem + i * SIZE_OF_SERIALIZED_FLOAT);
-		if (islessequal(delta[i], fabs(actual_val[i] - src_val[i])))
+		if (islessequal(delta[i], fabsf(actual_val[i] - src_val[i])))
 			TEST_FAILURE;
 		if (pnext != (actual_mem + SIZE_OF_SERIALIZED_FLOAT * (i + 1)))
 			TEST_FAILURE;
@@ -144,19 +144,152 @@ NEW_TEST(type_float_special)
 	}
 TEST_FINISHED
 
+#ifdef DOUBLE_SERIALIZATION_ALLOWED
+
+NEW_TEST(type_double_regular)
+	enum {N = 10};
+	double src_val[N] =
+		{  0.0       //  zero
+		,  1.0       //  one
+		, -1.0       //  minus one
+		,  1.0/3.0   //  0.(3)
+		,  M_PI      //  3.1415...
+		,  M_E       //  2.7182...
+		,  M_SQRT2   //  1.4142...
+		, -M_PI      // -3.1415...
+		, -M_E       // -2.7182...
+		, -M_SQRT2}; // -1.4142...
+	double delta[N];
+	double actual_val[N];
+	unsigned char actual_mem[SIZE_OF_SERIALIZED_DOUBLE * N];
+
+	// init delta
+	for (int i = 0; i < N; ++i)
+	{
+		delta[i] = fmax(5.0 * DBL_MIN, fabs(5.0 * DBL_EPSILON * src_val[i]));
+	}
+
+	// serialize
+	for (int i = 0; i < N; ++i)
+	{
+		void *pnext =
+			serialize_double(
+				  &(actual_mem[SIZE_OF_SERIALIZED_DOUBLE * i])
+				, src_val[i]);
+		if (pnext != (actual_mem + SIZE_OF_SERIALIZED_DOUBLE * (i + 1)))
+			TEST_FAILURE;
+	}
+
+	// deserialize
+	for (int i = 0; i < N; ++i)
+	{
+		void *pnext =
+			deserialize_double(
+				  &(actual_val[i])
+				, actual_mem + i * SIZE_OF_SERIALIZED_DOUBLE);
+		if (islessequal(delta[i], fabs(actual_val[i] - src_val[i])))
+			TEST_FAILURE;
+		if (pnext != (actual_mem + SIZE_OF_SERIALIZED_DOUBLE * (i + 1)))
+			TEST_FAILURE;
+	}
+TEST_FINISHED
+
+NEW_TEST(type_double_special)
+	void *p;
+	uint8_t indicate_byte;
+	double src_pinf    =  HUGE_VAL;
+	double src_ninf    = -HUGE_VAL;
+	double src_nan     =  nan("");
+	double src_regular =  1.234;
+	double actual_val;
+	unsigned char actual_mem[SIZE_OF_SERIALIZED_DOUBLE];
+
+	{ // check positive infinity
+		p = serialize_double(actual_mem, src_pinf);
+		// memory shift
+		if (p != (actual_mem + SIZE_OF_SERIALIZED_DOUBLE))
+			TEST_FAILURE;
+		// indicate byte
+		p = deserialize_uint8_t(&indicate_byte, actual_mem);
+		if (indicate_byte != FPMARK_PINF)
+			TEST_FAILURE;
+
+		p = deserialize_double(&actual_val, actual_mem);
+		// memory shift
+		if (p != (actual_mem + SIZE_OF_SERIALIZED_DOUBLE))
+			TEST_FAILURE;
+		// check value
+		if (!isinf(actual_val))
+			TEST_FAILURE;
+		if (!isgreater(actual_val, 0.0))
+			TEST_FAILURE;
+	}
+	{ // check positive infinity
+		p = serialize_double(actual_mem, src_ninf);
+		// memory shift
+		if (p != (actual_mem + SIZE_OF_SERIALIZED_DOUBLE))
+			TEST_FAILURE;
+		// indicate byte
+		p = deserialize_uint8_t(&indicate_byte, actual_mem);
+		if (indicate_byte != FPMARK_NINF)
+			TEST_FAILURE;
+
+		p = deserialize_double(&actual_val, actual_mem);
+		// memory shift
+		if (p != (actual_mem + SIZE_OF_SERIALIZED_DOUBLE))
+			TEST_FAILURE;
+		// check value
+		if (!isinf(actual_val))
+			TEST_FAILURE;
+		if (!isless(actual_val, 0.0))
+			TEST_FAILURE;
+	}
+	{ // check NaN
+		p = serialize_double(actual_mem, src_nan);
+		// memory shift
+		if (p != (actual_mem + SIZE_OF_SERIALIZED_DOUBLE))
+			TEST_FAILURE;
+		// indicate byte
+		p = deserialize_uint8_t(&indicate_byte, actual_mem);
+		if (indicate_byte != FPMARK_NAN)
+			TEST_FAILURE;
+
+		p = deserialize_double(&actual_val, actual_mem);
+		// memory shift
+		if (p != (actual_mem + SIZE_OF_SERIALIZED_DOUBLE))
+			TEST_FAILURE;
+		// check value
+		if (!isnan(actual_val))
+			TEST_FAILURE;
+	}
+	{ // check regular
+		p = serialize_double(actual_mem, src_regular);
+		// memory shift
+		if (p != (actual_mem + SIZE_OF_SERIALIZED_DOUBLE))
+			TEST_FAILURE;
+		// indicate byte
+		p = deserialize_uint8_t(&indicate_byte, actual_mem);
+		if (indicate_byte != FPMARK_REGULAR)
+			TEST_FAILURE;
+
+		p = deserialize_double(&actual_val, actual_mem);
+		// memory shift
+		if (p != (actual_mem + SIZE_OF_SERIALIZED_DOUBLE))
+			TEST_FAILURE;
+		// check value
+		if (isnan(actual_val))
+			TEST_FAILURE;
+		if (isinf(actual_val))
+			TEST_FAILURE;
+	}
+TEST_FINISHED
+#endif // DOUBLE_SERIALIZATION_ALLOWED
+
 INIT_TESTS_CHECKS
 	CHECK_TEST(TEST_CHECK_UP, type_float_regular);
 	CHECK_TEST(TEST_CHECK_UP, type_float_special);
-#ifdef INT16_SERIALIZATION_ALLOWED
-	//CHECK_TEST(TEST_CHECK_UP, type_int16_t);
-	//CHECK_TEST(TEST_CHECK_UP, type_uint16_t);
-#endif // INT16_SERIALIZATION_ALLOWED
-#ifdef INT32_SERIALIZATION_ALLOWED
-	//CHECK_TEST(TEST_CHECK_UP, type_int32_t);
-	//CHECK_TEST(TEST_CHECK_UP, type_uint32_t);
-#endif // INT32_SERIALIZATION_ALLOWED
-#ifdef INT64_SERIALIZATION_ALLOWED
-	//CHECK_TEST(TEST_CHECK_UP, type_int64_t);
-	//CHECK_TEST(TEST_CHECK_UP, type_uint64_t);
-#endif // INT64_SERIALIZATION_ALLOWED
+#ifdef DOUBLE_SERIALIZATION_ALLOWED
+	CHECK_TEST(TEST_CHECK_UP, type_double_regular);
+	CHECK_TEST(TEST_CHECK_UP, type_double_special);
+#endif // DOUBLE_SERIALIZATION_ALLOWED
 DONE_TESTS_CHECKS
