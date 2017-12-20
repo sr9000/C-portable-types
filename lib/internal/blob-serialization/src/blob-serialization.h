4255e8da24dd70ef7e9cc5c86c5b7caab358dda1
Rogonov Stepan <rogonovstepan@gmail.com>
2017-12-20 22:18:15 +0300

    Serialization blobs library

---
+++ lib/internal/blob-serialization/src/blob-serialization.h
@@ -0,0 +1,9 @@
+
+/**
+ * @file blob-serialization.h
+ * @author Rogonov Stepan Alekseevich
+ * @brief File containing definitions of serialization functions.
+ * @details Defines serialization and deserialization functions for Binary
+ * Large OBject.
+ * @warning There are no checks for unallocated memory or out-of-range errors.
+ */
